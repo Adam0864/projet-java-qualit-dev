378d0dd9d9c2ac166bf225b1b15e7b43dd165ec7
Adam <ahachicha4@outlook.fr>
2025-04-28 10:56:07 +0200

    java projet tp

--- src/test/java/PlageHoraireTest.java
+++ src/test/java/PlageHoraireTest.java
@@ -1,2 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
 public class PlageHoraireTest {
+    @org.junit.jupiter.api.Test
+    void Testcompareto() {
+        PlageHoraire P1 = new PlageHoraire(new Horaire(14,15),new Horaire(14,30));
+        PlageHoraire P2 = new PlageHoraire(new Horaire(15,15),new Horaire(15,30));
+        assertEquals(-1,new PlageHoraire(new Horaire(14,15),new Horaire(14,30)));
+    }
 }
