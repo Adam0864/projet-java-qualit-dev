f1bdfafc974a541eca63c5b1ec29a5bbb12765b6
Adam <ahachicha4@outlook.fr>
2025-04-28 10:14:08 +0200

    java projet tp

---
+++ src/test/java/HoraireTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+public class HoraireTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+    }
+}
